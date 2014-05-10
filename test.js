a0e34687e55c111d58d7c6b99bfcb0334fcf59a6
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-05-10 13:10:56 +0200

    Add example of adhoc() to test

--- test.js
+++ test.js
@@ -15,11 +15,14 @@ var person = function() {
 var chafe = require('./chafe');
 
 var mary = person();
-var hi = chafe(mary) // this: mary
-    .setName("Mary") // mary.setName()
-    .sayHi()         // mary.sayHi()
-    .chafe.pass()    // this: "Hi, I'm"
-    .toUpperCase()   // "Hi, I'm".toUpperCase()
-    .chafe.value();  // => "HI, I'M MARY"
+var hi = chafe(mary)               // this: mary
+    .setName("Mary")               // mary.setName()
+    .sayHi()                       // mary.sayHi()
+    .chafe.pass()                  // this: "Hi, I'm"
+    .toUpperCase()                 // "Hi, I'm Mary".toUpperCase()
+    .chafe.adhoc(function(punc) {
+      return this + punc;
+    }, "!")                        // "HI, I'M MARY".anon("!")
+    .chafe.value();                // => "HI, I'M MARY!"
 
 console.log(hi);
